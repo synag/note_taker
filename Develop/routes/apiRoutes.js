f7560f8cb695ea142793d889b649c1abee604eed
Carl Benbow <cbenbow@synergisticagility.com>
2021-05-09 13:25:06 -0400

    clean up of the files

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,5 @@
 const fs = require("fs");
-var shortid = require('shortid');
-
-
+var shortid = require("shortid");
 
 const db = require("../db/db.json");
 
@@ -11,27 +9,17 @@ module.exports = (app) => {
 
   console.log(db);
 
-
-
   app.post("/api/notes", (req, res) => {
-    // const obj = req.body;
-    const obj = req.body
-    obj.id = shortid.generate()
+    const obj = req.body;
+    obj.id = shortid.generate();
     db.push(obj);
-    console.log(db);
-    console.log(shortid.generate());
 
     res.json(obj);
   });
 
-  app.delete('/api/notes/:id', (req,res)=>{
-    console.log("req params", req.params.id)
-     indexPos = db.indexOf(req.params.id)
-     db.splice(indexPos,1)
-      res.send("Deleted")
-    })
-  
+  app.delete("/api/notes/:id", (req, res) => {
+    indexPos = db.indexOf(req.params.id);
+    db.splice(indexPos, 1);
+    res.send("Deleted");
+  });
 };
-
-
-
